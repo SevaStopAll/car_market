c6c1daf241f383703826ff447c7d9a04246e8bfa
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-22 16:58:20 +0400

    1. ToMany [#301848]

--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -10,5 +10,6 @@ create table auto_post
     id   serial primary key,
     description varchar not null,
     created timestamp not null,
-    auto_user_id int references auto_user(id) not null
+    auto_user_id int references auto_user(id) not null,
+    post_user_id int references auto_post(id) not null
 );
