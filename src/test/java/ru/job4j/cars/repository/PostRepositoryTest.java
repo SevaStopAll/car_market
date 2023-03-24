aacb8393175fe6fc4ad6e9f62b43296fd28947c5
SevaStopAll <sevastopallrh@yandex.ru>
2023-03-24 13:53:02 +0400

    2. Интеграционные тесты для проект job4j_cars [#4746]

---
+++ src/test/java/ru/job4j/cars/repository/PostRepositoryTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.cars.repository;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PostRepositoryTest {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    PostRepository post = new PostRepository(new CrudRepository(sf));
+}
